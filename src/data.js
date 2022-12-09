ae3055d6d0a8291d00fc386806e7e6435d0118cb
ABHIJEET MISHRA <106923752+AbhijeetXmishra@users.noreply.github.com>
2022-12-09 22:24:07 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,148 @@
+let book = [
+    {
+        title: "Book1",
+        description: "description1",
+        genre: "genre1",
+        author: "author1",
+        rating: 3,
+        review: "review1",
+        FavQuotes: "FavQuotes1",
+        section: '0'
+
+    },
+    {
+        title: "Book2",
+        description: "description2",
+        genre: "genre2",
+        author: "author2",
+        rating: 3,
+        review: "review2",
+        FavQuotes: "FavQuotes2",
+        section: '0'
+
+    },
+    {
+        title: "Book3",
+        description: "description3",
+        genre: "genre3",
+        author: "author3",
+        rating: 3,
+        review: "review3",
+        FavQuotes: "FavQuotes3",
+        section: '0'
+
+    },
+    {
+        title: "Book4",
+        description: "description4",
+        genre: "genre4",
+        author: "author4",
+        rating: 3,
+        review: "review4",
+        FavQuotes: "FavQuotes4",
+        section: '0'
+
+    },
+    {
+        title: "Book5",
+        description: "description5",
+        genre: "genre5",
+        author: "author5",
+        rating: 3,
+        review: "review5",
+        FavQuotes: "FavQuotes5",
+        section: '0'
+
+    },
+    {
+        title: "Book6",
+        description: "description6",
+        genre: "genre6",
+        author: "author6",
+        rating: 3,
+        review: "review6",
+        FavQuotes: "FavQuotes6",
+        section: '1'
+
+    },
+    {
+        title: "Book7",
+        description: "description7",
+        genre: "genre7",
+        author: "author7",
+        rating: 3,
+        review: "review7",
+        FavQuotes: "FavQuotes7",
+        section: '2'
+
+    },
+    {
+        title: "Book8",
+        description: "description8",
+        genre: "genre8",
+        author: "author8",
+        rating: 3,
+        review: "review8",
+        FavQuotes: "FavQuotes8",
+        section: '2'
+
+    },
+    {
+        title: "Book9",
+        description: "description9",
+        genre: "genre9",
+        author: "author9",
+        rating: 3,
+        review: "review9",
+        FavQuotes: "FavQuotes9",
+        section: '1'
+
+    },
+    {
+        title: "Book10",
+        description: "description10",
+        genre: "genre10",
+        author: "author10",
+        rating: 3,
+        review: "review10",
+        FavQuotes: "FavQuotes10",
+        section: '0'
+
+    },
+    {
+        title: "Book11",
+        description: "description11",
+        genre: "genre11",
+        author: "author11",
+        rating: 3,
+        review: "review11",
+        FavQuotes: "FavQuotes11",
+        section: '2'
+
+    },
+    {
+        title: "Book12",
+        description: "description12",
+        genre: "genre12",
+        author: "author12",
+        rating: 3,
+        review: "review12",
+        FavQuotes: "FavQuotes12",
+        section: '1'
+
+    },
+];
+let user = [
+    {
+        name: "Hitesh",
+        toRead: 'Book5,Book9',
+        isRead: 'Book2,Book3'
+    },
+    {
+        name: "Dinesh",
+        toRead: 'Book6,Book8',
+        isRead: 'Book2,Book1'
+    }
+]
+
+module.exports = { book, user };
